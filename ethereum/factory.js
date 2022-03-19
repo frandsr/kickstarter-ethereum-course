745999440a6e96f1101fbbdb908d6352212eea1c
Francisco de Sautu Riestra <fdesautu@gmail.com>
2022-03-19 16:30:22 -0300

    Update commit

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
-
+//Genero una instancia de un contrato ya existente
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
   "0x1137bd0DB2C019df4A791fab266291f20CF362Ef"
