ec59e9d4054e751299f2bcc538ea36f4260f5647
Francisco de Sautu Riestra <fdesautu@gmail.com>
2022-03-26 12:56:59 -0300

    some more staff

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,10 +1,49 @@
-import React from "react";
+import { React, useState } from "react";
 import Layout from "../../components/Layout";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 const CampaignNew = () => {
+  const [minimumContribution, setMinimumContribution] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    setLoading(true);
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(minimumContribution).send({
+        //Metamask en este caso aumaticamente trata de calcular el gas que se va a necesitar
+        from: accounts[0],
+      });
+    } catch (error) {
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Layout>
-      <h1>New Campaigns</h1>
+      <h3>Create a Campaign</h3>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
+        <Form.Field>
+          <label htmlFor="">Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
+            value={minimumContribution}
+            onChange={(e) => setMinimumContribution(e.target.value)}
+          />
+        </Form.Field>
+        <Message error header="Oops!" content={errorMessage}></Message>
+        <Button primary loading={loading}>
+          Create!
+        </Button>
+      </Form>
     </Layout>
   );
 };
